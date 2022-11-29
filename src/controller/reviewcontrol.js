dc68fcacd206ab7c95576ee564b666e3ffc7f042
tathagatamishra <tathagatamishra007@gmail.com>
2022-11-29 21:31:17 +0530

    working on review ctrl

--- src/controller/reviewcontrol.js
+++ src/controller/reviewcontrol.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const ReviewModel = require("../models/reviewmodel")
 const BookModel = require("../models/bookmodel")
 
@@ -5,6 +6,17 @@ exports.createReview = async (req, res) => {
     try {
 
         let bookId = req.path.bookId
+
+        let valid = mongoose.isValidObjectId(bookId)
+        if (valid == false) {
+            return res.status(400).send({ status: false, message: "Enter a valid book id" })
+        }
+
+        let bookData = await BookModel.findById(bookId)
+
+        await BookModel.updateOne({isDeleted: false, _id: bookId}, {$set: {reviews: bookData.reviews++}})
+
+
         let data = req.body
 
         let reviewData = await ReviewModel.create(data)
