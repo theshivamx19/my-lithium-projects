7d0668032b9e8a4b4d976555f8028ee8ed70223a
tathagatamishra <tathagatamishra007@gmail.com>
2022-12-02 17:54:00 +0530

    All done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
-const userCtrl = require("../controller/usercontrol")
-const bookCtrl = require("../controller/bookcontrol")
-const reviewCtrl = require("../controller/reviewcontrol")
-const authWare = require("../middleware/authware")
+const {createUser, userLogin} = require("../controller/usercontrol")
+const {createBook, filterBookByQuery, getBookById, updateBookById, deleteBookById} = require("../controller/bookcontrol")
+const {createReview, updateReview, deleteReview} = require("../controller/reviewcontrol")
+const {authentication, authorization} = require("../middleware/authware")
 
 
 router.get("/servertest", (req, res) => res.send("Server is working fine !"))
 
 
-router.post("/register", userCtrl.createUser)
-router.post("/login", userCtrl.userLogin)
+router.post("/register", createUser)
+router.post("/login",    userLogin)
 
 
-router.post("/books", authWare.authentication, authWare.authorization, bookCtrl.createBook)
-router.get("/books", authWare.authentication, bookCtrl.filterBookByQuery)
-router.get("/books/:bookId", authWare.authentication, bookCtrl.getBookById)
-router.put("/books/:bookId", authWare.authentication, authWare.authorization, bookCtrl.updateBookById)
-router.delete("/books/:bookId", authWare.authentication, authWare.authorization, bookCtrl.deleteBookById)
+router.post("/books",           authentication, authorization, createBook)
+router.get("/books",            authentication, filterBookByQuery)
+router.get("/books/:bookId",    authentication, getBookById)
+router.put("/books/:bookId",    authentication, authorization, updateBookById)
+router.delete("/books/:bookId", authentication, authorization, deleteBookById)
 
 
-router.post("/books/:bookId/review", reviewCtrl.createReview)
-router.put("/books/:bookId/review/:reviewId", reviewCtrl.updateReview)
-router.delete("/books/:bookId/review/:reviewId", reviewCtrl.deleteReview)
+router.post("/books/:bookId/review",             createReview)
+router.put("/books/:bookId/review/:reviewId",    updateReview)
+router.delete("/books/:bookId/review/:reviewId", deleteReview)
 
 
 module.exports = router
